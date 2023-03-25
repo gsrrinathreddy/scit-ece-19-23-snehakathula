7eb8d709eede518b53e3547a9cefb93b198d96ba
sneha kathula <snehakathula15@gmail.com>
2023-03-25 16:57:55 +0530

    mui added

---
+++ project/src/pages/Hobbies/index.js
@@ -0,0 +1,7 @@
+export default function Hobbies(){
+    return(
+        <>
+           <h5> This is hobbies page </h5>
+       </>
+    )
+}
---
+++ project/src/pages/Qualifications/index.js
@@ -0,0 +1,7 @@
+export default function Qualification(){
+    return(
+        <>
+           <h5> This is qualification page </h5>
+       </>
+    )
+}
